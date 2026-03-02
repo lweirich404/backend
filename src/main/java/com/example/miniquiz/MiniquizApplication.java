d11a6571abd040c114f70fa52bd0baecc3cb7c7f
Lisa <programming@weirich-kh.de>
2026-03-02 11:16:00 +0100

    change name

--- src/main/java/com/example/miniquiz/MiniquizApplication.java
+++ src/main/java/com/example/miniquiz/MiniquizApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.miniquiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MiniquizApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MiniquizApplication.class, args);
 	}
 
 }
